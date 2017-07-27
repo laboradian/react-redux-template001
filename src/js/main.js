0520bdda86ddd17a5db7114aea67e2d3267a9317
kura5 <laboradian@gmail.com>
2017-07-27 11:30:25 +0900

    Add console.log for laboradian.com

--- src/js/main.js
+++ src/js/main.js
@@ -90,3 +90,7 @@ render(
   document.getElementById('root')
 )
 
+console.log('%c 🌈 Laboradian.com 🌈 %c http://laboradian.com ',
+  'background: #2383BF; color: #fff; font-size: 1.4em;',
+  'background: #e3e3e3; color: #000; margin-bottom: 1px; padding-top: 4px; padding-bottom: 1px;');
+
